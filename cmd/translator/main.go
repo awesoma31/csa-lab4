0f715fda3f7afb2ffa82e7d2114302dc755e6037
awesoma31 <gwerty31102005@gmail.com>
2025-04-21 12:34:51 +0300

    (lexer) switch to different lexer parser approach

--- cmd/translator/main.go
+++ cmd/translator/main.go
@@ -1,21 +1,6 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
+import ()
 
 func main() {
-	fmt.Print("Enter expression: ")
-	scanner := bufio.NewScanner(os.Stdin)
-	if !scanner.Scan() {
-		fmt.Fprintln(os.Stderr, "No input")
-		os.Exit(1)
-	}
-	line := scanner.Text()
-	parser := NewMathParser(line)
-	ast := parser.ParseMath()
-	fmt.Println("\nAST:")
-	fmt.Print(ast.String(""))
 }
