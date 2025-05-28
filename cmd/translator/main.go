b8c3ae281066fd53737aa2b7468045d1d5669264
awesoma31 <gwerty31102005@gmail.com>
2025-05-28 21:00:13 +0300

    -

--- cmd/translator/main.go
+++ cmd/translator/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"encoding/binary"
 	"encoding/json"
 	"fmt"
 	"os"
@@ -34,35 +33,6 @@ func main() {
 	// fmt.Println(string(astJson))
 	fmt.Println(a)
 	// fmt.Println(string(prettyJson.String()))
-
-	//cg := NewCG()
-	//var dataMem []uint32 // собираем параллельно
-	//dataMem := make([]Word, 4096) // или сколько нужно
-	//cg := &CodeGen{
-	//	Sym:  NewSymTab(),
-	//	Data: &dataMem,
-	//}
-	//
-	//cg.Sym.nextData = 0x1000
-	//
-	//for _, stmt := range astTree.Body {
-	//	fmt.Println()
-	//	switch n := stmt.(type) {
-	//
-	//	case *ast.VarDeclarationStmt:
-	//		cg.genVarDecl(n)
-	//	default:
-	//		panic("only var decls in demo")
-	//	}
-	//}
-	//
-	//saveWords("program.bin", cg.Instr)
-	//saveWords("data.bin", dataMem)
-	//err = os.WriteFile("program.txt", []byte(strings.Join(cg.Report, "\n")), 0644)
-	//if err != nil {
-	//	return
-	//}
-	// litter.Dump(ast)
 }
 
 func getPrettyJson(in []byte) (string, error) {
@@ -72,12 +42,4 @@ func getPrettyJson(in []byte) (string, error) {
 		return "", err
 	}
 	return prettyJson.String(), nil
-}
-
-func saveWords(name string, words []uint32) {
-	f, _ := os.Create(name)
-	defer f.Close()
-	for _, w := range words {
-		_ = binary.Write(f, binary.LittleEndian, w)
-	}
 }
