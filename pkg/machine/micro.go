da7461aaf1805ffdeb56a0f816ec4d5564853701
awesoma31 <gwerty31102005@gmail.com>
2025-06-07 13:09:39 +0300

    -

--- pkg/machine/micro.go
+++ pkg/machine/micro.go
@@ -18,7 +18,6 @@ const (
 var ucode [opcodesMaxAmount][modesMaxAmount]func(int, int, int) microStep
 
 func init() {
-	// ─── NOP (single tick) ───────────────────────────────
 	ucode[isa.OpNop][isa.NoOperands] = uNop
 	ucode[isa.OpHalt][isa.NoOperands] = uHalt
 
@@ -26,6 +25,14 @@ func init() {
 	ucode[isa.OpMov][isa.MvMemReg] = uMovMemReg
 	ucode[isa.OpMov][isa.MvImmReg] = uMovImmReg
 	ucode[isa.OpMov][isa.MvRegReg] = uMovRegReg
+	ucode[isa.OpMov][isa.MvImmMem] = uMovImmMem
+	ucode[isa.OpMov][isa.MvRegMem] = uMovRegMem
+	ucode[isa.OpMov][isa.MvMemMem] = uMovMemMem
+
+	// JUMP BRENCH
+
+	// MATH
+	// CALL RET ...
 
 }
 
@@ -37,7 +44,7 @@ func uNop(_, _, _ int) microStep {
 	}
 }
 
-func uHalt(_ int, _ int, _ int) microStep {
+func uHalt(_, _, _ int) microStep {
 	return func(c *CPU) bool {
 		fmt.Println("simultaion stopped")
 		os.Exit(0)
@@ -45,36 +52,15 @@ func uHalt(_ int, _ int, _ int) microStep {
 	}
 }
 
-//	func uMovMemReg(rd, _, _ int) microStep {
-//		stage := 0
-//		var b [4]byte
-//		return func(c *CPU) bool {
-//			switch stage {
-//			case 0: // T0 – fetch 2-nd word (адрес)
-//				c.reg.PC++
-//				c.reg.GPR[isa.RAddr] = c.memI[c.reg.PC]
-//				stage++
-//			case 1: // T1 – read byte 0
-//				b[0] = c.memD[c.reg.GPR[isa.RAddr]]
-//				stage++
-//			case 2: // T2 – read byte 1
-//				b[1] = c.memD[c.reg.GPR[isa.RAddr]+1]
-//				stage++
-//			case 3: // T3 – read byte 2
-//				b[2] = c.memD[c.reg.GPR[isa.RAddr]+2]
-//				stage++
-//			case 4: // T4 – read byte 3
-//				b[3] = c.memD[c.reg.GPR[isa.RAddr]+3]
-//				stage++
-//			case 5: // T5 – assemble and write back
-//				val := uint32(b[0]) | uint32(b[1])<<8 | uint32(b[2])<<16 | uint32(b[3])<<24
-//				c.reg.GPR[rd] = val
-//				c.reg.PC++
-//				return true
-//			}
-//			return false
-//		}
-//	}
+func uMovMemMem(rd, _, _ int) microStep {
+	panic("unimplemeted")
+}
+func uMovImmMem(rd, _, _ int) microStep {
+	panic("unimplemeted")
+}
+func uMovRegMem(rd, _, _ int) microStep {
+	panic("unimplemeted")
+}
 func uMovMemReg(rd, _, _ int) microStep {
 	stage := 0
 	return func(c *CPU) bool {
@@ -103,7 +89,6 @@ func uMovImmReg(rd, _, _ int) microStep {
 		return true
 	}
 }
-
 func uMovRegReg(rd, rs1, _ int) microStep {
 	return func(c *CPU) bool {
 		c.reg.GPR[rd] = c.reg.GPR[rs1]
